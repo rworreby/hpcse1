9e104fd5701fc8506fa23fc1691ead38522bda36
Robin Worreby <rworreby@student.ethz.ch>
2020-10-09 11:32:46 +0200

    Implement row and col mat vec mul

--- exercise01/2_cache_matrices/matrix_vector.cpp
+++ exercise01/2_cache_matrices/matrix_vector.cpp
@@ -7,10 +7,17 @@
 std::vector<double> Ax_row(std::vector<double> &A, std::vector<double> &x){
 
     size_t N = x.size();
-    std::vector<double> y(N);
+    std::vector<double> y(N, 0);
 
-    //TOTO: Question 2a: Matrix vector multiplication with a row major matrix
-
+    for (size_t i = 0; i < N; i++) {
+        size_t const i_N{ i*N };
+        double tmp{ 0.0 };
+        double x_i{ x[i] };
+        for (size_t j = 0; j < N; j++) {
+            tmp += A[i_N + j] * x_i;
+        }
+        y[i] = tmp;
+    }
 
     return y;
 }
@@ -19,32 +26,33 @@ std::vector<double> Ax_row(std::vector<double> &A, std::vector<double> &x){
 std::vector<double> Ax_col(std::vector<double> &A, std::vector<double> &x){
 
     size_t N = x.size();
-    std::vector<double> y(N);
+    std::vector<double> y(N, 0);
 
-    //TOTO: Question 2a: Matrix vector multiplication with a column major matrix
-
+    for (size_t i = 0; i < N; i++) {
+        size_t const i_N{ i*N };
+        for (size_t j = 0; j < N; j++) {
+            y[j] += A[j*N + i] * x[j];
+        }
+    }
     return y;
 }
 
 
 double benchmark_Ax(std::vector<double> &A, std::vector<double> &x, bool row_major, double Ns){
-
     double times = 0;
 
-    for( size_t i=0; i<Ns; i++){
+    for(size_t i=0; i<Ns; i++){
         auto t1 = std::chrono::system_clock::now();
-        //TOTO: Question 2a: Call the function to be benchmarked
-        if( row_major==true ){
-
+        if(row_major==true){
+            Ax_row(A, x);
         }
         else{
-
+            Ax_col(A, x);
         }
         auto t2 = std::chrono::system_clock::now();
         times += std::chrono::duration<double>(t2-t1).count();
     }
     std::cout << "Done in total " << times << " -- average " << times/Ns << '\n';
-
 
     return times/Ns;
 }
@@ -63,9 +71,13 @@ int main(int argc, char const *argv[])
     size_t Ns = atoi(argv[2]);
     std::vector<double> A(N*N), B(N*N), x(N);
 
-    // TODO: Question 2a: Initialize matrices and vector
-    //       store A as row major and B as column major
-    
+    for (size_t i = 0; i < N; ++i) {
+        x[i] = i;
+        for (size_t j = 0; j < N; ++j) {
+            A[j*N + i] = i + j;
+            B[i*N + j] = i + j;
+        }
+    }
 
     std::cout << "Working with matrix of dimension " << N << std::endl;
 
