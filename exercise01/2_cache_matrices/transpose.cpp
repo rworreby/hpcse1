200e05f5af769b8b17252b7dff020ee026b3c8d8
Robin Worreby <rworreby@student.ethz.ch>
2020-10-12 15:48:24 +0200

    Initialize matrix and naive matrix transpose
    Rewrite outputformat to csv

--- exercise01/2_cache_matrices/transpose.cpp
+++ exercise01/2_cache_matrices/transpose.cpp
@@ -7,11 +7,14 @@
 
 
 void transpose(std::vector<double> A){
-
     size_t N = sqrt(A.size());
     std::vector<double> AT(N*N);
-    // TODO: Question 2b: Straightforward matrix transposition
 
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < N; ++j) {
+            AT[j*N + i] = A[i*N + j];
+        }
+    }
 }
 
 
@@ -29,7 +32,6 @@ double benchmark_transpose(std::vector<double> A, size_t mode, size_t block_size
     size_t N = sqrt(A.size());
     double times = 0;
 
-    // TODO: Check that the matrix size is divided by the block_size when mode==2
     if(mode==2 &&  N%block_size!=0){
         std::cout << "Error: the size of the matrix " << N
         << " should be divided by the block_size variable "
@@ -39,13 +41,14 @@ double benchmark_transpose(std::vector<double> A, size_t mode, size_t block_size
 
     for( size_t i=0; i<Ns; i++){
         auto t1 = std::chrono::system_clock::now();
-        // TODO: Question 2b: Call the function to be benchmarked
-        if(mode==1){
 
+        if(mode==1){
+            transpose(A);
         }
         else if(mode==2){
-
+            transpose_block(A, block_size);
         }
+
         auto t2 = std::chrono::system_clock::now();
         times += std::chrono::duration<double>(t2-t1).count();
     }
@@ -73,14 +76,19 @@ int main( )
     std::vector<double> A;
 
     // loop over matrix sizes
-    for( size_t m=0; m<M; m++){
+    for(size_t m=0; m<M; m++){
 
-        std::cout << "Working with a matrix of size " << matrix_size[m] << ".\n";
+        std::cout << "Working with a matrix of size "
+                  << matrix_size[m] << ".\n";
 
         size_t N = matrix_size[m];
+        A.resize(N*N);
 
-        // TODO: Initialize the matrix
-
+        for(size_t i = 0; i < N; ++i){
+            for(size_t j = 0; j < N; ++j){
+                A[i*N + j] = i*N + j;
+            }
+        }
 
         std::cout << "Start transposing (non optimized).\n";
         times1[m] = benchmark_transpose( A, 1, 0, Ns );
@@ -101,16 +109,16 @@ int main( )
     FILE *fp=nullptr;
     fp = fopen("transpose_times.txt","w");
     // write header to the file
-    std::string header = "# N   time_unoptimized ";
+    std::string header = "matrix_size,time_unoptimized";
     for(size_t b=0; b<B; b++){
-        header = header + "  block_" + std::to_string(block_size[b]);
+        header = header + ",block_" + std::to_string(block_size[b]);
     }
     header = header + "\n";
     fprintf(fp, "%s", header.c_str());
     for(size_t m=0; m<M; m++){
-        fprintf(fp, "%d %lf", matrix_size[m], times1[m]);
+        fprintf(fp, "%d,%lf", matrix_size[m], times1[m]);
         for(size_t b=0; b<B; b++){
-            fprintf(fp, " %lf ", times2[b][m]);
+            fprintf(fp, ",%lf", times2[b][m]);
         }
         fprintf(fp, "\n");
     }
