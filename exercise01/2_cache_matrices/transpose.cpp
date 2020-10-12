070ac601ccbe5a77d57304ce97b502e997219295
Robin Worreby <rworreby@student.ethz.ch>
2020-10-12 15:15:56 +0200

    Rewrite transpose to C++ style instead of C

--- exercise01/2_cache_matrices/transpose.cpp
+++ exercise01/2_cache_matrices/transpose.cpp
@@ -1,114 +1,118 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstdlib>
 #include <vector>
 #include <chrono>
-#include <math.h>
+#include <cmath>
 #include <string>
 
 
-void transpose( std::vector<double> A ){
+void transpose(std::vector<double> A){
 
-  size_t N = sqrt(A.size());
-  std::vector<double> AT(N*N);
-  // TODO: Question 2b: Straightforward matrix transposition
-
-}
-
-
-void transpose_block( std::vector<double> A, size_t blockSize ){
-
-  size_t N = sqrt(A.size());
-  std::vector<double> AT(N*N);
-  // TODO: Question 2b: Block matrix transposition
+    size_t N = sqrt(A.size());
+    std::vector<double> AT(N*N);
+    // TODO: Question 2b: Straightforward matrix transposition
 
 }
 
 
-double benchmark_transpose( std::vector<double> A, size_t mode, size_t blockSize, size_t Ns ){
+void transpose_block( std::vector<double> A, size_t block_size ){
 
-  size_t N = sqrt(A.size());
-  double times = 0;
+    size_t N = sqrt(A.size());
+    std::vector<double> AT(N*N);
+    // TODO: Question 2b: Block matrix transposition
 
-  // TODO: Check that the matrix size is divided by the blockSize when mode==2
-  if( mode==2 &&  N%blockSize!=0 ){
-    printf("Error: the size of the matrix (%zu) should be divided by the blockSize variable (%zu).\n",N,blockSize);
-    exit(1);
-  }
+}
 
-  for( size_t i=0; i<Ns; i++){
-    auto t1 = std::chrono::system_clock::now();
-    // TODO: Question 2b: Call the function to be benchmarked
-    if( mode==1 ){
 
+double benchmark_transpose( std::vector<double> A, size_t mode, size_t block_size, size_t Ns ){
+
+    size_t N = sqrt(A.size());
+    double times = 0;
+
+    // TODO: Check that the matrix size is divided by the block_size when mode==2
+    if(mode==2 &&  N%block_size!=0){
+        std::cout << "Error: the size of the matrix " << N
+        << " should be divided by the block_size variable "
+        << block_size << ".\n";
+        exit(1);
     }
-    else if( mode==2 ){
 
+    for( size_t i=0; i<Ns; i++){
+        auto t1 = std::chrono::system_clock::now();
+        // TODO: Question 2b: Call the function to be benchmarked
+        if(mode==1){
+
+        }
+        else if(mode==2){
+
+        }
+        auto t2 = std::chrono::system_clock::now();
+        times += std::chrono::duration<double>(t2-t1).count();
     }
-    auto t2 = std::chrono::system_clock::now();
-    times += std::chrono::duration<double>(t2-t1).count();
-  }
-  printf("Done in total %9.4fs  --  average %9.4fs\n", times, times/Ns);
+    std::cout << "Done in total " << times << " --  average "
+              << times/Ns << std::endl;
 
-  return times/Ns;
+    return times/Ns;
 
 }
 
 
 int main( )
 {
-  std::vector<int> matrixSize{ 1024, 2048, 4096 };
-  size_t M = matrixSize.size();
+    std::vector<int> matrix_size{ 1024, 2048, 4096 };
+    size_t M = matrix_size.size();
 
-  std::vector<size_t> blockSize{ 2, 4, 8, 16, 32, 64, 128 };
-  size_t B = blockSize.size();
+    std::vector<size_t> block_size{ 2, 4, 8, 16, 32, 64, 128 };
+    size_t B = block_size.size();
 
-  size_t Ns = 2;
+    size_t Ns = 2;
 
-  std::vector<double> times1(M);
-  std::vector< std::vector<double>> times2(B, std::vector<double>(M) );
+    std::vector<double> times1(M);
+    std::vector< std::vector<double>> times2(B, std::vector<double>(M) );
 
-  std::vector<double> A;
+    std::vector<double> A;
 
-  // loop over matrix sizes
-  for( size_t m=0; m<M; m++){
+    // loop over matrix sizes
+    for( size_t m=0; m<M; m++){
 
-    printf("Working with a matrix of size %d\n",matrixSize[m]);
+        std::cout << "Working with a matrix of size " << matrix_size[m] << ".\n";
 
-    size_t N = matrixSize[m];
+        size_t N = matrix_size[m];
 
-    // TODO: Initialize the matrix
+        // TODO: Initialize the matrix
 
 
-    printf("Start transposing (non optimized).\n");
-    times1[m] = benchmark_transpose( A, 1, 0, Ns );
+        std::cout << "Start transposing (non optimized).\n";
+        times1[m] = benchmark_transpose( A, 1, 0, Ns );
 
-    // loop over block sizes
-    for( size_t b=0; b<B; b++){
+        // loop over block sizes
+        for(size_t b=0; b<B; b++){
 
-      printf("Start transposing (optimized, block size=%zu).\n", blockSize[b] );
-      times2[b][m] = benchmark_transpose( A, 2, blockSize[b], Ns );
+            std::cout << "Start transposing (optimized, block size="
+                      << block_size[b] << ".\n";
+            times2[b][m] = benchmark_transpose(A, 2, block_size[b], Ns);
+        }
+
+        std::cout << "==================================================\n";
     }
 
-    printf("==================================================\n");
-  }
 
-
-  // write results to a file
-  FILE *fp=nullptr;
-  fp = fopen("transpose_times.txt","w");
-  // write header to the file
-  std::string header = "# N   time_unoptimized ";
-  for(size_t b=0; b<B; b++)
-    header = header + "  block_" + std::to_string(blockSize[b]);
-  header = header + "\n";
-  fprintf(fp,"%s",header.c_str());
-  for(size_t m=0; m<M; m++){
-    fprintf(fp,"%d %lf",matrixSize[m],times1[m]);
+    // write results to a file
+    FILE *fp=nullptr;
+    fp = fopen("transpose_times.txt","w");
+    // write header to the file
+    std::string header = "# N   time_unoptimized ";
     for(size_t b=0; b<B; b++)
-      fprintf(fp," %lf ",times2[b][m]);
-    fprintf(fp,"\n");
-  }
-  fclose(fp);
+    header = header + "  block_" + std::to_string(block_size[b]);
+    header = header + "\n";
+    fprintf(fp,"%s",header.c_str());
+    for(size_t m=0; m<M; m++){
+        fprintf(fp,"%d %lf",matrix_size[m],times1[m]);
+        for(size_t b=0; b<B; b++)
+        fprintf(fp," %lf ",times2[b][m]);
+        fprintf(fp,"\n");
+    }
+    fclose(fp);
 
-  return 0;
+    return 0;
 }
